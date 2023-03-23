fb8a997761b8619360ddf612b9211231c3cc8cdc
AbdullahEmadd <abdullah.emad@excp.sa>
2023-03-23 15:11:33 +0200

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -53,8 +53,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(360,690),
-      minTextAdapt: true,
-      splitScreenMode: true,
+      minTextAdapt: false,
+      splitScreenMode: false,
       builder: (context, child) {
         return GetMaterialApp(
             locale: const Locale("ar"),
