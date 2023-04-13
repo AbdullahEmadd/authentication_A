cc9b5db03dc46a781a38d80f02ed231c8493c30d
AbdullahEmadd <abdullah.emad@excp.sa>
2023-04-13 10:52:17 +0200

    ..

--- packages/manager/lib/src/screens/manager_screens/products_screens/get_products_screen/get_products_screen.dart
+++ packages/manager/lib/src/screens/manager_screens/products_screens/get_products_screen/get_products_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:manager/src/components/custom_app_bar/custom_app_bar.dart';
+import 'package:manager/src/components/custom_empty_data/custom_empty_data.dart';
 import 'package:manager/src/components/custom_floating_action/custom_floating_action.dart';
 import 'package:manager/src/components/custom_text/custom_text.dart';
 import 'package:manager/src/components/loader_custom/loader_custom.dart';
@@ -42,79 +43,95 @@ class _GetProductsScreenState extends State<GetProductsScreen> {
           GenericState<List<ProductsModel>>>(
             bloc: getProductsViewModel.getProductsModel,
             builder: (context, state) {
-              return ListView.builder(
-                itemCount: getProductsViewModel.getProductsModel.state.data!.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10.w),
-                    child: SizedBox(
-                      height: 82.h,
-                      child: Card(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            getProductsViewModel.getProductsModel.state.data![index].prodImages!.isNotEmpty?
-                            Container(
-                              height: 82,
-                              width: MediaQuery.of(context).size.width / 3,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: NetworkImage(
-                                      AppConsts.imageDomain+ getProductsViewModel
+              if (state is GenericUpdate) {
+                return state.data!.isNotEmpty?
+                ListView.builder(
+                  itemCount: getProductsViewModel.getProductsModel.state.data!
+                      .length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10.w),
+                      child: SizedBox(
+                        height: 82.h,
+                        child: Card(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              getProductsViewModel.getProductsModel.state
+                                  .data![index].prodImages!.isNotEmpty ?
+                              Container(
+                                height: 82,
+                                width: MediaQuery
+                                    .of(context)
+                                    .size
+                                    .width / 3,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: NetworkImage(
+                                        AppConsts.imageDomain +
+                                            getProductsViewModel
+                                                .getProductsModel
+                                                .state
+                                                .data![index].prodImages![0]
+                                                .url!
+                                    ),),
+                                  borderRadius: const BorderRadius.only(
+                                    bottomRight: Radius.circular(5),
+                                    topRight: Radius.circular(5),
+                                  ),
+                                  color: AppColors.gray,
+                                ),
+                              ) : Container(width: MediaQuery
+                                  .of(context)
+                                  .size
+                                  .width / 3,),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  CustomText(
+                                    text: 'Name: ${getProductsViewModel
+                                        .getProductsModel
+                                        .state
+                                        .data![index]
+                                        .name!}',
+                                    maxLines: 2,
+                                    fontSize: 14.sp,
+                                    color: AppColors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  getProductsViewModel
                                       .getProductsModel
                                       .state
-                                      .data![index].prodImages![0].url!
-                                  ),),
-                                borderRadius: const BorderRadius.only(
-                                  bottomRight: Radius.circular(5),
-                                  topRight: Radius.circular(5),
-                                ),
-                                color: AppColors.gray,
+                                      .data![index]
+                                      .description != null ?
+                                  CustomText(
+                                    text: 'Description: ${getProductsViewModel
+                                        .getProductsModel
+                                        .state
+                                        .data![index]
+                                        .description!}',
+                                    maxLines: 2,
+                                    fontSize: 14.sp,
+                                    color: AppColors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ) : Container(),
+                                ],
                               ),
-                            ): Container(width: MediaQuery.of(context).size.width / 3,),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                CustomText(
-                                  text: 'Name: ${getProductsViewModel
-                                      .getProductsModel
-                                      .state
-                                      .data![index]
-                                      .name!}',
-                                  maxLines: 2,
-                                  fontSize: 14.sp,
-                                  color: AppColors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                getProductsViewModel
-                                    .getProductsModel
-                                    .state
-                                    .data![index]
-                                    .description != null ?
-                                CustomText(
-                                  text: 'Description: ${getProductsViewModel
-                                      .getProductsModel
-                                      .state
-                                      .data![index]
-                                      .description!}',
-                                  maxLines: 2,
-                                  fontSize: 14.sp,
-                                  color: AppColors.black,
-                                  fontWeight: FontWeight.bold,
-                                ): Container(),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 10.h,
-                            ),
-                          ],
+                              SizedBox(
+                                height: 10.h,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                },
-              );
+                    );
+                  },
+                ):
+                const CustomEmptyData();
+              } else {
+                return Container();
+              }
             },
           ),
           floatingActionButton: CustomFloatingAction(
